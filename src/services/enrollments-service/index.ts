80b7fe3e5e705c7110de492e1b665c030f7c7210
Kássia <kassia_guterres@outlook.com>
2022-11-12 22:41:22 -0300

    /enrollments/cep route implemented and CEP validation at postCreateOrUpdateEnrollment function

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -4,13 +4,22 @@ import addressRepository, { CreateAddressParams } from "@/repositories/address-r
 import enrollmentRepository, { CreateEnrollmentParams } from "@/repositories/enrollment-repository";
 import { exclude } from "@/utils/prisma-utils";
 import { Address, Enrollment } from "@prisma/client";
+import { AddressByCEP } from "protocols.js";
 
-async function getAddressFromCEP() {
-  const result = await request.get("https://viacep.com.br/ws/37440000/json/");
-
+async function getAddressFromCEP(cep: string) {
+  const result = await request.get(`https://viacep.com.br/ws/${cep}/json/`);
+ 
+  const address: AddressByCEP = {
+    logradouro: result.data.logradouro, 
+    complemento: result.data.complemento, 
+    bairro: result.data.bairro, 
+    cidade: result.data.localidade, 
+    uf: result.data.uf
+  }; 
   if (!result.data) {
     throw notFoundError();
   }
+  return address;
 }
 
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
@@ -40,8 +49,14 @@ type GetAddressResult = Omit<Address, "createdAt" | "updatedAt" | "enrollmentId"
 async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollmentWithAddress) {
   const enrollment = exclude(params, "address");
   const address = getAddressForUpsert(params.address);
+  
+  const searchCEP = await request.get(`https://viacep.com.br/ws/${address.cep}/json/`);
+  const CEPValidation = searchCEP.data.cep;
 
-  //TODO - Verificar se o CEP é válido
+  if(!CEPValidation) {
+    throw requestError(400, "This CEP is not valid");
+  }
+
   const newEnrollment = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, "userId"));
 
   await addressRepository.upsert(newEnrollment.id, address, address);
